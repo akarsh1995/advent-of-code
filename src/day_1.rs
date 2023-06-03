18781d4b93a34b5a376a066f6942c8af6cfa5ecd
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-06-03 11:05:45 +0530

    day_1p2

--- src/day_1.rs
+++ src/day_1.rs
@@ -20,6 +20,23 @@ fn sol_1p1(x: &str) -> u32 {
     max
 }
 
+fn sol_1p2(x: &str) -> u32 {
+    let splitted = x.split("\n");
+    let mut current_elf = 0;
+    let mut v = vec![];
+    for line in splitted {
+        if line == "" {
+            v.push(current_elf);
+            current_elf = 0
+        } else {
+            current_elf += line.parse::<u32>().unwrap();
+        }
+    }
+    v.push(current_elf);
+    v.sort();
+    return v[v.len() - 3..v.len()].iter().sum();
+}
+
 #[test]
 fn solution_1p1() {
     let x: String =
@@ -31,4 +48,11 @@ fn solution_1p1() {
 fn test_1p1() {
     let x = "5\n6\n7\n\n5";
     assert_eq!(sol_1p1(x), 18);
+}
+
+#[test]
+fn test_1p2() {
+    let x: String =
+        read_to_string("/home/akarshj/Programming/advent_of_code/data/input_1").unwrap();
+    println!("Answer: {:?}", sol_1p2(&x));
 }
