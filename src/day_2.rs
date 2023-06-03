7277a7d0253db6dfd77e425deddf107e61d5b3f2
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-06-03 12:42:06 +0530

    day_2p2

--- src/day_2.rs
+++ src/day_2.rs
@@ -86,3 +86,80 @@ fn test_day_2p1() {
     let some_string: String = std::fs::read_to_string("./data/day_2p1").unwrap();
     println!("{}", calc_total_score(&some_string));
 }
+
+struct Action<'a>(&'a str);
+
+struct Input2<'a>(&'a str);
+
+fn get_win_move(opponents_move: &Move) -> Move {
+    match opponents_move {
+        Move::Rock => Move::Paper,
+        Move::Paper => Move::Scissors,
+        Move::Scissors => Move::Rock,
+    }
+}
+
+fn get_losing_move(opponents_move: &Move) -> Move {
+    match opponents_move {
+        Move::Rock => Move::Scissors,
+        Move::Paper => Move::Rock,
+        Move::Scissors => Move::Paper,
+    }
+}
+
+impl<'a> Input2<'a> {
+    fn target(&self) -> OutCome {
+        let s = self.0.split(" ");
+        match s.last().unwrap() {
+            "Y" => OutCome::Draw,
+            "X" => OutCome::Loss,
+            "Z" => OutCome::Win,
+            _ => panic!("Wrong outcome"),
+        }
+    }
+
+    fn opponent_move(&self) -> Move {
+        let mut s = self.0.split(" ");
+        match s.next().unwrap() {
+            "A" => Move::Rock,
+            "B" => Move::Paper,
+            "C" => Move::Scissors,
+            _ => panic!("Wrong move"),
+        }
+    }
+
+    fn get_target_move(&self) -> Move {
+        match self.target() {
+            OutCome::Win => get_win_move(&self.opponent_move()),
+            OutCome::Loss => get_losing_move(&self.opponent_move()),
+            OutCome::Draw => self.opponent_move(),
+        }
+    }
+
+    fn get_score(&self) -> u32 {
+        let my_move = &self.get_target_move();
+        my_move.get_win_score(&self.opponent_move())
+    }
+}
+
+#[test]
+fn test_day_2p2_sample() {
+    let sample_string = "A Y\nB X\nC Z";
+    let mut s = 0;
+    for k in sample_string.split("\n") {
+        s += Input2(k).get_score();
+    }
+    assert_eq!(12, s);
+}
+
+#[test]
+fn test_day_2p2() {
+    let some_string: String = std::fs::read_to_string("./data/day_2p1").unwrap();
+    let mut s = 0;
+    for k in some_string.split("\n") {
+        if k != "" {
+            s += Input2(k).get_score();
+        }
+    }
+    println!("test_day_2p2: {}", s);
+}
