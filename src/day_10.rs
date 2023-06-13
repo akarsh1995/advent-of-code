9220c769daff8b594deaeb174266be62853b0caf
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-06-13 10:19:19 +0530

    day 10 p 2

--- src/day_10.rs
+++ src/day_10.rs
@@ -1,4 +1,6 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, fmt::Display};
+
+use itertools::Itertools;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Instruction {
@@ -37,6 +39,18 @@ struct CPU {
     register_x: i32,
     instructions: VecDeque<Instruction>,
     cycles: u32,
+    display: Vec<Vec<char>>,
+}
+
+impl Display for CPU {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let k = self
+            .display
+            .iter()
+            .map(|c| c.iter().collect::<String>())
+            .join("\n");
+        write!(f, "{k}")
+    }
 }
 
 impl CPU {
@@ -50,11 +64,33 @@ impl CPU {
             cycles: 0,
             register_x: 1,
             instructions: inss,
+            display: (0..6)
+                .map(|_| (0..40).map(|_| '.').collect::<Vec<char>>())
+                .collect::<Vec<Vec<char>>>(),
         }
     }
 
     fn decode(&mut self) {
         self.current = self.instructions.pop_front().map(|i| (i, i.cycles()));
+    }
+
+    fn draw(&mut self) {
+        let nth_row = ((self.cycles / 40) % 6) as usize;
+        let display_row = &mut self.display[nth_row];
+        if self.register_x == -1 {
+            display_row[(self.cycles % 40) as usize] = '#';
+        }
+        if self.register_x >= 0 && self.register_x <= 39 {
+            let c_mask = cycle_mask(self.cycles % 40);
+            let s_mask = sprite_value(self.register_x as u32);
+            if (c_mask & s_mask) > 0 {
+                display_row[(self.cycles % 40) as usize] = '#';
+            } else {
+                display_row[(self.cycles % 40) as usize] = '.';
+            }
+        } else {
+            display_row[(self.cycles % 40) as usize] = '.';
+        }
     }
 
     fn step(&mut self) -> bool {
@@ -79,6 +115,53 @@ impl CPU {
 
     fn get_running_cycle(&self) -> u32 {
         self.cycles
+    }
+}
+
+const DISPLAY_MASK: u64 = 0b1111111111111111111111111111111111111111;
+
+fn cycle_mask(cycle: u32) -> u64 {
+    (0b1000000000000000000000000000000000000000 >> (cycle % 40)) & DISPLAY_MASK
+}
+
+fn sprite_value(pos: u32) -> u64 {
+    (0b11100000000000000000000000000000000000000 >> pos) & DISPLAY_MASK
+}
+
+#[cfg(test)]
+mod bit_tests {
+
+    pub const INPUT: &'static str = include_str!("../data/year_2022__day_10");
+    use super::*;
+
+    #[test]
+    fn test_display() {
+        let cpu = CPU::new(INPUT);
+        println!("{}", cpu);
+    }
+
+    #[test]
+    fn test() {
+        assert_eq!(
+            format!("{:040b}", sprite_value(0)),
+            "1100000000000000000000000000000000000000"
+        );
+        assert_eq!(
+            format!("{:040b}", sprite_value(1)),
+            "1110000000000000000000000000000000000000"
+        );
+        assert_eq!(
+            format!("{:040b}", sprite_value(38)),
+            "0000000000000000000000000000000000000111"
+        );
+        assert_eq!(
+            format!("{:040b}", sprite_value(39)),
+            "0000000000000000000000000000000000000011"
+        );
+        assert_eq!(
+            format!("{:040b}", sprite_value(40)),
+            "0000000000000000000000000000000000000001"
+        );
     }
 }
 
@@ -141,5 +224,40 @@ mod tests {
             };
         }
         dbg!(ss);
+    }
+
+    #[test_case(include_str!("../data/sample_day_10"); "test_cpu_instructions_display_p_2_sample")]
+    fn test_cpu_instructions_display_p_2_sample(input: &str) {
+        // let mut ss: i32 = 0;
+        // let mut next_cycle = 20;
+        let mut cpu = CPU::new(input);
+        cpu.draw();
+        cpu.decode();
+        loop {
+            if !cpu.step() {
+                break;
+            } else {
+                cpu.draw();
+            }
+        }
+        println!("{}", &cpu);
+    }
+
+    #[test_case(include_str!("../data/year_2022__day_10"); "test_cpu_instructions_display_p_2")]
+    fn test_cpu_instructions_display_p_2(input: &str) {
+        // let mut ss: i32 = 0;
+        // let mut next_cycle = 20;
+        let mut cpu = CPU::new(input);
+        cpu.draw();
+        cpu.decode();
+        loop {
+            if !cpu.step() {
+                break;
+            } else {
+                cpu.draw();
+            }
+        }
+        println!("{}", &cpu);
+        // dbg!(ss);
     }
 }
