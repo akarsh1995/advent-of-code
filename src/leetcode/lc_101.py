5bc178ff4dbf8c67c5080ac6d6d143c64dbe137a
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-07-03 12:26:11 +0530

    fix: reduced space complexity

--- src/leetcode/lc_101.py
+++ src/leetcode/lc_101.py
@@ -10,31 +10,16 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def are_symmetric(v: List[Optional[TreeNode]]) -> bool:
-
-    if not any(v):
+def are_symmetric(node1: Optional[TreeNode], node2: Optional[TreeNode]) -> bool:
+    if node1 and not node2:
+        return False
+    elif not node1 and node2:
+        return False
+    elif not node1 and not node2:
         return True
+    else:
+        return node1.val == node2.val and are_symmetric(node1.right, node2.left) and are_symmetric(node1.left, node2.right)
 
-    flag = True
-
-    for elem_from_left, elem_from_right in zip(v, v[::-1]):
-        if elem_from_left and elem_from_right:
-            if elem_from_left.val != elem_from_right.val:
-                flag &= False
-        elif elem_from_left is None and elem_from_right is None:
-            flag &= True
-        else:
-            return False
-
-    all_child = []
-    for n in v:
-        if n:
-            all_child.append(n.left)
-            all_child.append(n.right)
-        else:
-            all_child.append(None)
-
-    return flag and are_symmetric(all_child)
 
 def test_compare(): 
     root1 = TreeNode(1)
@@ -46,7 +31,7 @@ def test_compare():
     root1.right.right = TreeNode(3)
     root1.right.left = TreeNode(4)
 
-    assert are_symmetric([root1])
+    assert are_symmetric(root1.left, root1.right)
 
     root1 = TreeNode(1)
     root1.left = TreeNode(5)
@@ -57,4 +42,21 @@ def test_compare():
     root1.right.right = TreeNode(3)
     root1.right.left = TreeNode(4)
 
-    assert are_symmetric([root1]) == False
+    assert not are_symmetric(root1.left, root1.right)
+
+def test_falling():
+    root1 = TreeNode(1)
+    root1.left = TreeNode(2)
+    root1.left.left = TreeNode(2)
+
+    root1.right = TreeNode(2)
+    root1.right.left = TreeNode(2)
+
+    assert not are_symmetric(root1.left, root1.right)
+
+def test_123():
+    root1 = TreeNode(1)
+    root1.left = TreeNode(2)
+    root1.right= TreeNode(3)
+
+    assert not are_symmetric(root1.right, root1.left)
