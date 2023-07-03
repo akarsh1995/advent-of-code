cc5f27200e94403e1dd2711e3e0a25dcde70436f
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-07-03 12:37:12 +0530

    lc 104

---
+++ src/leetcode/lc_104.py
@@ -0,0 +1,26 @@
+# 101. Symmetric Tree
+# Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+# Definition for a binary tree node.
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def max_depth(node: Optional[TreeNode]) -> int:
+    if not node:
+        return 0
+    return 1 + max(max_depth(node.left), max_depth(node.right))
+
+
+def test_compare(): 
+    root1 = TreeNode(3)
+    root1.left = TreeNode(9)
+    root1.right = TreeNode(20)
+    root1.right.right = TreeNode(7)
+    root1.right.left = TreeNode(15)
+    assert max_depth(root1) == 3
+
