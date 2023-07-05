a6671b69bdfdfb7ee0544594ee93af1d4ca56d9f
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-07-06 00:15:42 +0530

    lc 22 clean solution

--- src/leetcode/lc_22.py
+++ src/leetcode/lc_22.py
@@ -6,17 +6,15 @@ from typing import List
 OPEN = '('
 CLOSE = ')'
 
-def gen_par(g: List[str], temp: str, o: int = 0, c: int = 0, n: int = 3):
-    if o == n:
-        if c < n:
-            gen_par(g, temp + CLOSE, o, c + 1, n)
-        else:
-            g.append(temp)
-    elif o == c:
-        gen_par(g, temp + OPEN, o + 1, c, n)
-    elif o > c:
-        gen_par(g, temp + OPEN, o + 1, c, n)
-        gen_par(g, temp + CLOSE, o, c + 1, n)
+def gen_par(g: List[str], p: str, o: int = 0, c: int = 0, n: int = 3):
+    if o == c == n:
+        g.append(p)
+        return
+    if o < n:
+        gen_par(g, p + OPEN, o + 1, c, n)
+    if c < o:
+        gen_par(g, p + CLOSE, o, c+1, n)
+
 
 def test():
     n = 3
