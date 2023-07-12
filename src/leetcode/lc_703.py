1013deb92fbe74d8fcee3e18404e662d4db93181
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-07-12 17:44:45 +0530

    lc 703

---
+++ src/leetcode/lc_703.py
@@ -0,0 +1,54 @@
+# 703. Kth Largest Element in a Stream
+# Easy
+
+# Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+# Implement KthLargest class:
+
+#     KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+#     int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+
+#
+
+# Example 1:
+
+# Input
+# ["KthLargest", "add", "add", "add", "add", "add"]
+# [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+# Output
+# [null, 4, 5, 5, 8, 8]
+
+# Explanation
+# KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+# kthLargest.add(3);   // return 4
+# kthLargest.add(5);   // return 5
+# kthLargest.add(10);  // return 5
+# kthLargest.add(9);   // return 8
+# kthLargest.add(4);   // return 8
+
+
+
+
+from typing import List, Optional
+from tree import TreeNode
+from bisect import bisect_left
+
+
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]) -> None:
+        self.k = k
+        self.nums = nums
+        self.nums.sort()
+
+    def add(self, val: int) -> int:
+        self.nums.insert(bisect_left(self.nums, val), val)
+        return self.nums[-self.k]
+
+
+def test():
+    kth_largest = KthLargest(3, [4,5,8,2])
+    assert kth_largest.add(3) == 4
+    assert kth_largest.add(5) == 5
+    assert kth_largest.add(10) == 5
+    assert kth_largest.add(9) == 8
+    assert kth_largest.add(4) == 8
