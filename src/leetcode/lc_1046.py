b236798fcf557c73bd85ff9b3b1cd07a611208a1
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-07-12 22:15:58 +0530

    lc 1046

---
+++ src/leetcode/lc_1046.py
@@ -0,0 +1,66 @@
+# 1046. Last Stone Weight
+# Easy
+
+# You are given an array of integers stones where stones[i] is the weight of the ith stone.
+
+# We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+
+#     If x == y, both stones are destroyed, and
+#     If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+
+# At the end of the game, there is at most one stone left.
+
+# Return the weight of the last remaining stone. If there are no stones left, return 0.
+
+#
+
+# Example 1:
+
+# Input: stones = [2,7,4,1,8,1]
+# Output: 1
+# Explanation:
+# We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+# we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+# we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+# we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
+
+# Example 2:
+
+# Input: stones = [1]
+# Output: 1
+
+from typing import List
+
+def bsearch(arr: List[int], val: int) -> int:
+
+    condition = lambda m: arr[m] >= val
+
+    l = 0
+    r = len(arr)
+
+    while l < r:
+        m = l + (r - l) // 2
+
+        if condition(m):
+            r = m
+        else:
+            l = m + 1
+
+    return l
+
+
+def get_last(arr: List[int]) -> int:
+    arr.sort()
+
+    while len(arr) >= 2:
+        y, x = arr.pop(), arr.pop()
+        arr.insert(bsearch(arr, y - x), y - x)
+
+    return arr[0]
+
+def test():
+    arr = [2,7,4,1,8,1]
+    assert get_last(arr) == 1
+
+    arr = [1]
+    assert get_last(arr) == 1
